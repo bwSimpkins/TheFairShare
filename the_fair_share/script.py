6ec283735359ecaf1ec6870884d92e0937db5c99
Boyer Simpkins <106032615+bwSimpkins@users.noreply.github.com>
2025-02-08 12:57:44 -0600

    added formatting of currency

--- the_fair_share/script.py
+++ the_fair_share/script.py
@@ -1,7 +1,14 @@
+import locale
+
+# Adds commas to the values and rounds decimals
+def format_currecy_value(value):
+    locale.setlocale(locale.LC_ALL, '')
+    return locale.currency(value, symbol=False, grouping=True)
+
 def process_multiple_inputs(inputs):
     try:
         numbers = [float(value) for value in inputs.values()]  # Convert inputs to floats
         total_sum = sum(numbers)  # Calculate sum
-        return f"Sum of inputs: {total_sum}"
+        return f"Household Income: ${format_currecy_value(total_sum)}"
     except ValueError:
         return "Please enter valid numbers."
