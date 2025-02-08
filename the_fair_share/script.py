55dbed13b3842f3f8eb07a4584ddd0f98eb59d97
Boyer Simpkins <106032615+bwSimpkins@users.noreply.github.com>
2025-02-08 15:00:42 -0600

    added ability to output multiple lines into the result box

--- the_fair_share/script.py
+++ the_fair_share/script.py
@@ -5,10 +5,12 @@ def format_currecy_value(value):
     locale.setlocale(locale.LC_ALL, '')
     return locale.currency(value, symbol=False, grouping=True)
 
+# Calculates the 
+
 def process_multiple_inputs(inputs):
     try:
         numbers = [float(value) for value in inputs.values()]  # Convert inputs to floats
         total_sum = sum(numbers)  # Calculate sum
-        return f"Household Income: ${format_currecy_value(total_sum)}"
+        return f"Household Income: ${format_currecy_value(total_sum)}" + "\n hello"
     except ValueError:
         return "Please enter valid numbers."
